d6fc13918e1b5f867bd5f59334184d93b89ae3db
Taky0ndon <mmaggio89@gmail.com>
2025-08-02 18:11:21 -0400

    added self join method

--- 181_EmployeesEarningMoreThanTheirManagers.sql
+++ 181_EmployeesEarningMoreThanTheirManagers.sql
@@ -4,3 +4,8 @@
 -- get name where  s1 > s2
 SELECT name as "Employee" FROM Employee e
     WHERE e.salary > (SELECT salary FROM Employee f WHERE f.id = e.managerId);
+    
+-- Selfjoin approach
+-- SELECT e.name as "Employee" from Employee e
+JOIN Employee m ON e.managerId = m.id
+WHERE e.salary > m.salary;
