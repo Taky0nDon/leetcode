89ee4e1249168e2c80ba0a43d2ce05e11c10224b
Taky0ndon <mmaggio89@gmail.com>
2025-08-12 17:05:22 -0400

    improved big-O from log(m)*n to log(m)*log(n)

--- 181_EmployeesEarningMoreThanTheirManagers.sql
+++ 181_EmployeesEarningMoreThanTheirManagers.sql
@@ -8,4 +8,4 @@ SELECT name as "Employee" FROM Employee e
 -- Selfjoin approach
 -- SELECT e.name as "Employee" from Employee e
 JOIN Employee m ON e.managerId = m.id
-WHERE e.salary > m.salary;
+WHERE e.salary > m.sudo slary;
--- 74_SearchA2dMatrix.py
+++ 74_SearchA2dMatrix.py
@@ -4,12 +4,17 @@ class Solution:
         t = len(matrix) - 1
         while b <= t:
             m = (b + t) // 2
-            if matrix[m][0] == target:
-                return True
+            if matrix[m][0] == target: return True
             elif matrix[m][0] > target:
                 t = m - 1
             elif matrix[m][0] < target:
-                if target in matrix[m]: return True
-                else:
-                    b = m + 1
+                L = 0
+                R = len(matrix[m]) - 1
+                while L <= R:
+                    row_mid = int((L + R) / 2)
+                    if matrix[m][row_mid] == target: return True
+                    elif matrix[m][row_mid] < target:
+                        L = row_mid + 1
+                    else: R = row_mid - 1
+                b = m + 1
         return False
